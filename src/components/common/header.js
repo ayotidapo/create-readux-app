de4e73619f4ec27358cb9ba8305453bdc8b84a13
ayotidapo <otidapson@gmail.com>
2018-03-29 18:15:33 +0100

    dapson added

--- src/components/common/header.js
+++ src/components/common/header.js
@@ -5,7 +5,7 @@ import Header3 from "./header3";
 
 const header = () => (
   <div>
-    <h1 className="text-primary">The Header</h1>
+    <h1 className="text-primary">The Header of Dapson</h1>
     <Route path="/innerhead" component={Header3} />
     <Route path="/" component={AnotherHeader} />
   </div>
