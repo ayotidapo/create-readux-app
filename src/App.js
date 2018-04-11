111317ea3599af31bbf7ad45c581e0e595f8bed8
ayotidapo <otidapson@gmail.com>
2018-04-11 11:12:56 +0100

    remname

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React, { Component } from "react";
-import Routes from "./routes";
+import AllRoutes from "./routes";
 
 import "./App.css";
 
 class App extends Component {
   render() {
-    return <Routes />;
+    return <AllRoutes />;
   }
 }
 
